ddc782fa1cb41293ac9076d32f693a80cf2f122e
Niklas <me@sodapop.se>
2018-09-12 15:01:37 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 const withTypescript = require("@zeit/next-typescript");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const withImages = require("next-images");
+const withCSS = require("@zeit/next-css");
 
 module.exports = {
-  ...withTypescript(
-    withImages({
-      webpack(config, options) {
-        // Do not run type checking twice:
-        if (options.isServer)
-          config.plugins.push(new ForkTsCheckerWebpackPlugin());
+  ...withCSS(
+    withTypescript(
+      withImages({
+        webpack(config, options) {
+          // Do not run type checking twice:
+          if (options.isServer)
+            config.plugins.push(new ForkTsCheckerWebpackPlugin());
 
-        return config;
-      }
-    })
+          return config;
+        }
+      })
+    )
   ),
   exportPathMap: async function(defaultPathMap) {
     return {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 
 import logo from "../images/logo.png";
+import "./index.css";
 
 export default () => (
   <div className="Index">
     <Head>
       <title>Chainsaw Design</title>
-      <link rel="stylesheet" href="/static/main.css" type="text/css" />
+      <link rel="stylesheet" href="/_next/static/style.css" />
     </Head>
-    <img src={logo} />
+    <img src={logo} draggable={false} />
   </div>
 );
