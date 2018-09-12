bed34a8d431ecd36e795b26a1d1bcdc3d3506f3b
Niklas <me@sodapop.se>
2018-09-12 17:52:12 +0200

    Dark browser is cool browser

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,10 @@ export default () => (
     <Head>
       <title>Chainsaw Design</title>
       <link rel="stylesheet" href="/_next/static/style.css" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#000" />
+      <meta name="msapplication-navbutton-color" content="#000" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="#000" />
     </Head>
     <img src={logo} draggable={false} />
   </div>
