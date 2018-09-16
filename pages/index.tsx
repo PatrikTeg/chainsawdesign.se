39ec2c1bbdaf9608b3017909bcc67fc42f5c814c
Niklas <me@sodapop.se>
2018-09-16 20:38:59 +0200

    Fix scaling issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default () => (
     <Head>
       <title>Chainsaw Design</title>
       <link rel="stylesheet" href="/_next/static/style.css" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=0.5" />
       <meta name="theme-color" content="#000" />
       <meta name="msapplication-navbutton-color" content="#000" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black" />
