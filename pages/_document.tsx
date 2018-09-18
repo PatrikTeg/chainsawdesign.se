915be88e9623ac96bd981db711768952ca677adf
Niklas <me@sodapop.se>
2018-09-18 14:55:35 +0200

    Add favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { Head, Main, NextScript } from "next/document"
+
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>Chainsaw Design</title>
+          <link rel="stylesheet" href="/_next/static/style.css" />
+          <meta name="viewport" content="width=device-width, initial-scale=0.5" />
+          <meta name="theme-color" content="#000" />
+          <meta name="msapplication-navbutton-color" content="#000" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+          <link rel="manifest" href="/static/site.webmanifest" />
+          <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,10 @@
-import Head from "next/head";
+import logo from "../images/logo.png"
+import "./index.css"
 
-import logo from "../images/logo.png";
-import "./index.css";
-
-const Logo = () => <img src={logo} draggable={false} />;
+const Logo = () => <img src={logo} draggable={false} />
 
 export default () => (
   <div className="Index">
-    <Head>
-      <title>Chainsaw Design</title>
-      <link rel="stylesheet" href="/_next/static/style.css" />
-      <meta name="viewport" content="width=device-width, initial-scale=0.5" />
-      <meta name="theme-color" content="#000" />
-      <meta name="msapplication-navbutton-color" content="#000" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-    </Head>
     <Logo />
   </div>
-);
+)
