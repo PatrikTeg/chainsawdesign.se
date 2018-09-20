915cbafb8aeac41b6c50ea5c1cb3cfbc3cef959e
Niklas <me@sodapop.se>
2018-09-20 15:54:42 +0200

    Add instagram feed background

--- next.config.js
+++ next.config.js
@@ -1,25 +1,62 @@
-const withTypescript = require("@zeit/next-typescript");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const withImages = require("next-images");
-const withCSS = require("@zeit/next-css");
+const withTypescript = require("@zeit/next-typescript")
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
+const withImages = require("next-images")
+const withCSS = require("@zeit/next-css")
+
+const withJSONLoader = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      const { isServer } = options
+      if (!options.defaultLoaders) {
+        throw new Error(
+          "This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade"
+        )
+      }
+
+      const assetPrefix = nextConfig.assetPrefix || ""
+
+      config.module.rules.push({
+        test: /\.json$/,
+        use: [
+          {
+            loader: "json-loader",
+            options: {
+              fallback: "file-loader",
+              publicPath: `${assetPrefix}/_next/static/`,
+              outputPath: `${isServer ? "../" : ""}static/`,
+              name: "[name]-[hash].[ext]",
+            },
+          },
+        ],
+      })
+
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options)
+      }
+
+      return config
+    },
+  })
+}
 
 module.exports = {
   ...withCSS(
-    withTypescript(
-      withImages({
-        webpack(config, options) {
-          // Do not run type checking twice:
-          if (options.isServer)
-            config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    withJSONLoader(
+      withImages(
+        withTypescript({
+          webpack(config, options) {
+            // Do not run type checking twice:
+            if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
 
-          return config;
-        }
-      })
+            return config
+          },
+        })
+      )
     )
   ),
   exportPathMap: async function(defaultPathMap) {
     return {
-      "/": { page: "/" }
-    };
-  }
-};
+      "/": { page: "/" },
+    }
+  },
+}
